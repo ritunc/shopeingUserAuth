1c5f5fedeacccb551051654e24b9d23e41b2ba05
Ritun Chakraborty <mithynead@gmail.com>
2025-04-12 04:08:13 +0530

    1nd commit

--- src/LogIn.js
+++ src/LogIn.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-        origin:"http://localhost:3000",
+        origin:"https://shopeing-rituncs-projects.vercel.app",
         method: ["GET", "POST", "PUT", "DELETE"],
         credentials:true
 }));
